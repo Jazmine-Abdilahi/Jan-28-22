409309d419d416b39a0fbcbf63b00b5421986393
Jazmine Abdilahi <66798977+Jazmine-Abdilahi@users.noreply.github.com>
2022-01-28 16:08:25 -0600

    Add files via upload

---
+++ Jan28.js
@@ -0,0 +1,12 @@
+// Fri Jan 28
+
+Yaz.calcBIM();
+Jaz.calcBIM();
+
+console.log(Yaz.BIM > Jaz.BIM)
+
+
+if (Yaz.bim > Jaz.BIM) {
+    console.log(`${Yaz.fullname}`s BIM(${ Yaz.BIM }) is higher than ${ Jaz.fullname }'s BIM ${}Jaz.BIM')
+} else if (Jaz.bim > Yaz.BIM) {
+    console.log(`${Jaz.fullname}`s BIM(${ Jaz.BIM }) is higher than ${ Yaz.fullname }'s BIM ${}Yaz.BIM');
